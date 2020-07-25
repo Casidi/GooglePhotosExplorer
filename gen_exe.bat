8f7d25966c01fb5c000913422a1aeb1822a64d33
Jeff Home PC <tcfsh22215@gmail.com>
2020-07-25 23:00:12 +0800

    Adjust copy path

--- gen_exe.bat
+++ gen_exe.bat
@@ -1,3 +1,3 @@
 pyinstaller --hidden-import pkg_resources.py2_warn -F -w google_photos_gui.py --icon=google-photos.ico
-cp google-photos.ico dist/
-cp photos_api_secret.json dist/
+cp google-photos.ico .\dist\
+cp photos_api_secret.json .\dist\
